d8e509b5f4788f8663fdc3d2b4f791e4b13d9239
diego manuel guerrero torres <diegomgt11@gmail.com>
2021-01-31 17:36:33 -0500

    variables_Constantes_20_01_2021

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+vaso = 'café';
+console.log(vaso);
+vaso = 'agua';
+console.log(vaso)
+
+
+
+
+var nombre = "Diego guerrero";
+let edad = 36;
+
+var profesor = false;
+var lista_cursos = ['JavaScript', 'PHP'];
+
+var informacio_personal = { 
+    id: 939459035,
+    dirrecion: "Calle 34",
+};
+
+console.log(nombre);
+console.log(edad);
+console.log(profesor);
+console.log(lista_cursos);
+console.log(informacio_personal);
+
+
+const cedula = 91459696
+
+console.warn(cedula);
